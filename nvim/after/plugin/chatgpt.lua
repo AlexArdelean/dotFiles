0616c993cbb03211f31eda649dfaa6911f6c0e9f
Alex Ardelean <alex.ardelean@southie.ai>
2024-02-25 22:54:40 -0500

    Increase tokens in chatgpt

--- nvim/after/plugin/chatgpt.lua
+++ nvim/after/plugin/chatgpt.lua
@@ -3,7 +3,7 @@ require("chatgpt").setup({
     model = "gpt-4-0125-preview",
     frequency_penalty = 0,
     presence_penalty = 0,
-    max_tokens = 300,
+    max_tokens = 4096,
     temperature = 0,
     top_p = 1,
     n = 1,
