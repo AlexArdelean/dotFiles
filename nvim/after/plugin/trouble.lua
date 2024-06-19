70b3fa2c3ea9e17ec28195287fa9502e7710a616
Alex Ardelean <alex.ardelean@southie.ai>
2024-06-19 18:35:24 -0400

    Update packages and fix trouble

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,11 +1,10 @@
-
-require("trouble").setup{
-    severity = vim.diagnostic.severity.ERROR,
+require("trouble").setup {
+  filter = { severity = vim.diagnostic.severity.ERROR }
 }
 
 
 -- vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>t", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>t", function() require("trouble").toggle("diagnostics") end)
 -- vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
 -- vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
 -- vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
