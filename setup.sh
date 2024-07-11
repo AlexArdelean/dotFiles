dd07ff6117a7b50d0d2bef06cb89e34c55faa3e6
Alex Ardelean <alexandardelean@gmail.com>
2024-07-10 21:15:38 -0700

    Fix my entire setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 ln -s ~/dotFiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/dotFiles/nvim/ ~/.config/nvim/
+ln -s ~/dotFiles/nvim/ ~/.config/
+ln -s ~/dotFiles/alacritty/ ~/.config/
+ln -s ~/dotFiles/bashrc ~/.bashrc
