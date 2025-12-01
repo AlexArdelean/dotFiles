3a89c4d982d606b5c0af9d75649f86f1fa9dcba9
Alex Ardelean <alexandardelean@gmail.com>
2025-12-01 17:54:18 -0500

    Update my plugins and also fix broken plugins

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -1,35 +1,50 @@
-local lspconfig = require("lspconfig")
-local capabilities = require('blink.cmp').get_lsp_capabilities()
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-vim.lsp.enable('css_variables')
+-- CSS Variables
+vim.lsp.enable("css_variables")
 
-lspconfig.gopls.setup({
-  capabilities = capabilities
+-- Go
+vim.lsp.config("gopls", {
+	capabilities = capabilities,
+	root_markers = { "go.mod", "go.work", ".git" },
 })
 
--- Latex
-lspconfig.ltex.setup({})
-lspconfig.texlab.setup {}
+-- LaTeX - ltex
+vim.lsp.config("ltex", {
+	root_markers = { ".git" },
+})
+
+-- LaTeX - texlab
+vim.lsp.config("texlab", {
+	root_markers = { ".latexmkrc", ".git" },
+})
+
+-- Tailwind CSS
+vim.lsp.config("tailwindcss", {
+	capabilities = capabilities,
+	root_markers = { "tailwind.config.js", "tailwind.config.ts", "postcss.config.js", ".git" },
+	settings = {
+		tailwindCSS = {
+			experimental = {
+				classRegex = {
+					{ "cva\\(((?:[^()]|\\([^()]*\\))*)\\)", '["`]([^"\'`]*).*?["\'`]' },
+					{ "cx\\(((?:[^()]|\\([^()]*\\))*)\\)", "(?:'|\"`)([^']*)(?:'|\"|`)" },
+					{ "tv\\(([^)]*)\\)", '["`]([^"\'`]*).*?["\'`]' },
+					{ "tv\\(.*?\\).*`([^`]*)`", "([a-zA-Z0-9\\-:]+)" },
+				},
+			},
+		},
+	},
+})
+
+-- Enable all configured servers
+vim.lsp.enable({ "gopls", "ltex", "texlab", "tailwindcss" })
+
+-- LaTeX filetype settings
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "tex",
-  callback = function()
-    vim.opt.wrap = true
-    vim.opt.linebreak = true
-  end
-})
-
-lspconfig["tailwindcss"].setup({
-  capabilities = capabilities,
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        classRegex = {
-          { "cva\\(((?:[^()]|\\([^()]*\\))*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-          { "cx\\(((?:[^()]|\\([^()]*\\))*)\\)",  "(?:'|\"|`)([^']*)(?:'|\"|`)" },
-          { "tv\\(([^)]*)\\)",                    "[\"'`]([^\"'`]*).*?[\"'`]" },
-          { "tv\\(.*?\\).*?`([^`]*)`",            "([a-zA-Z0-9\\-:]+)" }
-        },
-      },
-    }
-  }
+	pattern = "tex",
+	callback = function()
+		vim.opt.wrap = true
+		vim.opt.linebreak = true
+	end,
 })
--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -4,32 +4,32 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sync_root_with_cwd = true,
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-    adaptive_size = true,
-    float = {
-      enable = false,
-    }
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-    exclude = { "logs", ".env" },
-  },
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-    ignore_list = {},
-  },
-  diagnostics = {
-    enable = true,
-  },
+	sync_root_with_cwd = true,
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 30,
+		adaptive_size = true,
+		float = {
+			enable = false,
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+		exclude = { "logs", ".env" },
+	},
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+		ignore_list = {},
+	},
+	diagnostics = {
+		enable = true,
+	},
 })
 
 vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
