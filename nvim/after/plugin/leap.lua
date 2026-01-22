ad28fec72f544bded14cf89d0921cbd60906fba1
Alex Ardelean <alexandardelean@gmail.com>
2026-01-22 11:45:28 -0500

    Update and ignore case for leap

--- nvim/after/plugin/leap.lua
+++ nvim/after/plugin/leap.lua
@@ -1,4 +1,7 @@
-require("leap").opts.case_sensitive = false
+leap = require("leap")
+
+leap.opts.case_sensitive = false
+leap.opts.vim_opts["go.ignorecase"] = true
 
 vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
 vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
