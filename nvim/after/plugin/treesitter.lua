b88204418e21d8866beec3210b0f126346c5a077
Alex Ardelean <alexandardelean@gmail.com>
2026-04-02 12:17:18 -0400

    Fix nvim treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,40 +1,18 @@
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "vimdoc", "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "css" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<CR>',
-      scope_incremental = '<CR>',
-      node_incremental = '<TAB>',
-      node_decremental = '<S-TAB>',
-    },
-  },
-}
+-- Enable treesitter highlighting for all filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
 
 function set_treesitter_cwd()
   local current_buffer_path = vim.fn.expand(":TSBufName")
   if current_buffer_path ~= '' then
     local current_directory = vim.fn.fnamemodify(current_buffer_path, ":h")
     vim.cmd('cd ' .. vim.fn.fnameescape(current_directory))
-    -- print('CWD changed to: ' .. current_directory)
-  else
-    -- print('Error: Unable to determine current buffer path.')
   end
 end
 
--- Add an autocmd to call the function when TreeSitter is loaded
 vim.api.nvim_exec([[
   augroup SetTreeSitterCWD
       autocmd!
