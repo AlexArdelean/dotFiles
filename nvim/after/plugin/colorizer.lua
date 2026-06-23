e3f84227fb13223bb1d6a24b5a32698654109af7
Alex Ardelean <alexandardelean@gmail.com>
2026-06-23 09:34:08 -0400

    Prevent lsp restart from erroring on colorizer

--- nvim/after/plugin/colorizer.lua
+++ nvim/after/plugin/colorizer.lua
@@ -1,4 +1,13 @@
 vim.o.termguicolors = true
+
+-- Neovim 0.12 enables native LSP document-color highlighting by default, but it
+-- crashes (document_color.lua:225 assertion) when on_lines fires a request for a
+-- stale client id after the server restarts. We use colorizer for swatches anyway,
+-- so disable the redundant native feature.
+if vim.lsp.document_color and vim.lsp.document_color.enable then
+  pcall(vim.lsp.document_color.enable, false)
+end
+
 require('colorizer').setup({
   -- 'css';
   -- 'javascript';
